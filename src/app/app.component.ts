4ac79eca94519bf48e63a57d51deb9369f418d83
DESKTOP-O4RCKL8\User <fabien@marmelade-app.fr>
2022-08-26 17:59:28 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+import * as M from "materialize-css";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'pokemon';
+
+  constructor() {
+
+  }
+
+  ngAfterViewInit() {
+    M.AutoInit()
+  }
+
+
+  logout() {
+
+  }
 }
